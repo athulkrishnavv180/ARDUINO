32a01eac725eff8dd30513ad4959e7c97f48d36d
athulkrishnavv180 <148170847+athulkrishnavv180@users.noreply.github.com>
2024-06-24 23:17:36 +0530

    Add files via upload
    code to blink leds at a specific intervals

---
+++ traffic light code.ino
@@ -0,0 +1,24 @@
+int LED1=5;
+int LED2=6;
+int LED3=7;
+void setup() {
+ pinMode(LED1,OUTPUT);
+ pinMode(LED2,OUTPUT);
+ pinMode(LED3,OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(LED1,HIGH);
+ delay(3000);
+  digitalWrite(LED1,LOW);
+ 
+  digitalWrite(LED2,HIGH);
+  delay(3000);
+  digitalWrite(LED2,LOW);
+  
+ digitalWrite(LED3,HIGH);
+  delay(3000);
+  digitalWrite(LED3,LOW);
+ 
+}
