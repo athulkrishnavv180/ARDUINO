e238994802e79180a82620661f3cbb491e5e8725
athulkrishnavv180 <148170847+athulkrishnavv180@users.noreply.github.com>
2024-06-26 14:05:29 +0530

    Add files via upload

---
+++ Temperature sensor and buzzer.ino
@@ -0,0 +1,30 @@
+const int tempsensorpin = A0;
+const int buzzerpin = 8;
+const float temperaturethreshold=5.0;
+
+void setup() {
+  
+ Serial.begin(9600);
+ pinMode(buzzerpin,OUTPUT);
+digitalWrite(buzzerpin,LOW);
+
+}
+
+void loop() {
+  int sensorvalue = analogRead(tempsensorpin);
+  float temperature = sensorvalue*(3.0/1023.0)*10;
+  Serial.print("temperature:");
+  Serial.print(temperature);
+Serial.println("c");
+
+
+  if(temperature>temperaturethreshold)
+  {
+    digitalWrite(buzzerpin,HIGH);
+   
+  }
+  else{
+    digitalWrite(buzzerpin,LOW);
+    }
+    delay(1000);
+}
